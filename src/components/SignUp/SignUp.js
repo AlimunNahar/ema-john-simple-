46db8155fae6070e6a9bc275d99ac23ef811c820
AlimunNahar <alimunnaharmitu@gmail.com>
2022-10-21 02:27:40 +0600

    login function added

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../contexts/UserContext";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [error, setError] = useState(null);
+  const { createUser } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+
+    // console.log(email, password, confirm);
+
+    if (password.length < 6) {
+      setError("Password should be 6 character or more!");
+    } else if (password !== confirm) {
+      setError("Your Password did not match!");
+      return;
+    }
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div className="form-container">
-      <h2 className="form-title">Login</h2>
-      <form>
+      <h2 className="form-title">Sign Up</h2>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="" required />
@@ -14,10 +43,15 @@ const SignUp = () => {
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="" required />
         </div>
-        <input className="btn-submit" type="submit" value="Login" />
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
         <p>
-          New to ema john? Please <Link to="/signup">Create a New Account</Link>
+          Already have an account? Please <Link to="/login">Login</Link>
         </p>
+        <p className="text-error">{error}</p>
       </form>
     </div>
   );
